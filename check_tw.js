2aa0604f399c6d66f74887d46cebba8d48faf5a0
tevaalon <32883463+tevaalon@users.noreply.github.com>
2024-01-28 11:00:49 -0500

    Update check_tw.js

--- check_tw.js
+++ check_tw.js
@@ -1,5 +1,60 @@
-console.log("Trigger Warnings");
 
+const twButton = document.createElement("button"); 
+const ul = document.getElementsByClassName("work navigation actions")[0];
+const popuptext = document.createElement("div");
+
+twButton.id = "tw-button";
+twButton.innerText = "Check for Trigger Warnings";
+twButton.style.position = "relative"; 
+twButton.style.display = "inline-block";
+twButton.style.cursor = "pointer";
+
+popuptext.id = "my-popup";;
+popuptext.setAttribute("class","popuptext");
+popuptext.style.visibility = "hidden"; 
+popuptext.style.textWrap = "wrap";
+popuptext.style.backgroundColor = "#900"; 
+popuptext.style.color = "#fff";
+popuptext.style.textAlign = "center";
+popuptext.style.borderRadius = "6px";
+popuptext.style.padding = "8px 0";
+popuptext.style.position = "absolute";
+popuptext.style.bottom = "fit-content";
+popuptext.style.left = "50%";
+popuptext.style.marginLeft = "-80px";
+
+twButton.appendChild(popuptext);
+ul.appendChild(twButton);
+
+//document.body.appendChild(twButton);
+//const twButton = document.getElementById("tw-button");
+document.getElementById("tw-button").onclick = function(){
+    return findTW();
+}
+
+function findTW(){
+    var popup = document.getElementById("my-popup");
+    if (popup.style.visibility == "hidden"){
+        popup.style.visibility = "visible";
+    }
+    else  popup.style.visibility = "hidden";
+    //popup.classList.toggle("show");
+
+    const notes = document.getElementsByClassName("notes module");
+    popup.innerText = "No TW found!"
+    for (var j=0; j<notes.length; j++){
+        const p = notes[j].getElementsByTagName("p");
+        for(var i = 0; i < p.length; i++){
+            const text = p[i].innerText
+            if (text.includes("TW") || text.includes("CW") || text.includes("warning")){
+                if (popup.innerText == "No TW found!") {
+                    popup.innerText = "";
+                }
+                popup.innerText += text;
+            }
+        }
+    }
+}
 
 function createButton(theID, buttonText) {
 
@@ -11,39 +66,6 @@ function createButton(theID, buttonText) {
     return name;
 
 }
-
-const ul = document.getElementsByClassName("work navigation actions")[0];
-twButton = createButton("tw-button", "Check for Trigger Warnings");
-
-const twResult = document.createElement("p");
-twResult.id = "tw-result";
-twResult.innerText = "";
-ul.appendChild(twResult);
-
-document.getElementById("tw-button").onclick = function(){
-    return findTW();
-}
-
-function findTW(){
-    const twResult = document.getElementById("tw-result");
-    const notes = document.getElementsByClassName("notes module");
-    //console.log(notes);
-    for (var j=0; j<notes.length; j++){
-        const p = notes[j].getElementsByTagName("p");
-        console.log(p);
-        for(var i = 0; i < p.length; i++){
-            const text = p[i].innerText;
-            if (text.includes("TW") || text.includes("CW") || text.includes("warning")){
-                twResult.innerText += text;
-            }
-        }
-    }
-    if (twResult.innerText == ""){
-        twResult.innerText = "No TW found!";
-    }
-}
-
-console.log("Toggle Images On and Off");
 
 function imageManagement(x) {
     userDocs = document.querySelectorAll('[class="userstuff"], [class="userstuff module"]'); // need to get the areas in which there are user control
@@ -86,6 +108,7 @@ function hide(image) {
 x = 1;
 textBubble = "Hide Images";
 imgButton = createButton("img-button", textBubble);
+imgButton.style.display = 'inline';
 
 imgButton.onclick = function(){
     imageManagement(x);
