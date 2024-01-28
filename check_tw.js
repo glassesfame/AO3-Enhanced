6cad4ac92a7130def6df1b4b1efb7f087272f417
tevaalon <32883463+tevaalon@users.noreply.github.com>
2024-01-28 08:22:45 -0500

    Update check_tw.js

--- check_tw.js
+++ check_tw.js
@@ -1,10 +1,18 @@
 console.log("Trigger Warnings")
 const twButton = document.createElement("button"); 
+const ul = document.getElementsByClassName("work navigation actions")[0];
+const twResult = document.createElement("p");
+
 twButton.id = "tw-button";
 twButton.innerText = "Check for Trigger Warnings";
-document.body.appendChild(twButton);
+twResult.id = "tw-result";
+twResult.innerText = "";
+
+ul.appendChild(twButton);
+ul.appendChild(twResult);
+//document.body.appendChild(twButton);
 //const twButton = document.getElementById("tw-button");
-twButton.onclick = function(){
+document.getElementById("tw-button").onclick = function(){
     return findTW();
 }
 
@@ -17,7 +25,7 @@ function findTW(){
         console.log(p)
         for(var i = 0; i < p.length; i++){
             const text = p[i].innerText
-            if (text.includes("TW") || text.includes("CW")){
+            if (text.includes("TW") || text.includes("CW") || text.includes("warning")){
                 twResult.innerText += text;
             }
         }
@@ -26,56 +34,3 @@ function findTW(){
         twResult.innerText = "No TW found!"
     }
 }
-
-console.log("Toggle Images On and Off");
-
-
-function imageManagement(x) {
-
-    userDocs = document.querySelectorAll('[class="userstuff"]'); // need to get the areas in which there are user control
-    for (let docs of userDocs) { // looping through the different areas
-        let imageList = docs.getElementsByTagName('img'); // getting the images in those areas
-        for (let image of imageList) { // acting on each of those images individually
-            decideAndAct(image, x);
-        };
-    };
-};
-
-function decideAndAct(image, x) {
-    // if x is odd, then we are hiding; if x is even, then we will show.
-    console.log(x)
-    if (x % 2 == 0) {
-        show(image);
-        textBubble = 'Hide Images';
-    } else {
-        console.log(image)
-        hide(image);
-        textBubble = 'Show Images';  
-    };
-    
-};
-
-
-// the action functions that we can take:
-function show(image) { 
-    image.style.display = 'inline';
-};
-
-function hide(image) { 
-    image.style.display = 'none';
-};
-
-
-// the button and going forward
-x = 1;
-
-const imgButton = document.createElement("button"); 
-imgButton.id = "img-button";
-imgButton.innerText = 'Hide Images';
-document.body.appendChild(imgButton);
-
-imgButton.onclick = function(){
-    imageManagement(x);
-    imgButton.innerText = textBubble;
-    x = x + 1;
-};
