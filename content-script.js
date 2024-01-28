55afe667fd4017491e1071fed581ca1c5cb53ecd
littleTunaa <157905323+littleTunaa@users.noreply.github.com>
2024-01-27 22:00:07 -0500

    Add files via upload

--- content-script.js
+++ content-script.js
@@ -1,1 +1,11 @@
 console.log("Content script is running on this page.")
+
+const complete = document.createElement("button"); 
+complete.id = "complete-button";
+complete.innerText = "Sort for completed works";
+document.body.appendChild(complete);
+
+const incomplete = document.createElement("button"); 
+incomplete.id = "incomplete-button";
+incomplete.innerText = "Sort for incomplete works";
+document.body.appendChild(incomplete);
